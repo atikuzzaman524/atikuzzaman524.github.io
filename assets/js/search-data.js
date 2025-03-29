3b539a3e305bab0806066c04a0368c28451c510b
atikuzzaman524 <18502839+atikuzzaman524@users.noreply.github.com>
2025-03-29 19:38:55 +0000

    Deploying to gh-pages from @ atikuzzaman524/atikuzzaman524.github.io@dd69969f9fb2435d26bb760e33be5cceabb4f26a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "",
+          description: "Academic Courses and Teaching Materials",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
