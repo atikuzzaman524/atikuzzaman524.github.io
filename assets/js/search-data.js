0d93fc266345472eaecd494d3ae7021337009775
atikuzzaman524 <18502839+atikuzzaman524@users.noreply.github.com>
2025-04-19 16:53:57 +0000

    Deploying to gh-pages from @ atikuzzaman524/atikuzzaman524.github.io@3775fca4428ce08920613155d1bda78d1ec9f4a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,6 +433,11 @@ ninja.data = [{
           description: "An introduction to machine learning/ techniques, covering preprocessing, pattern mining, classification, clustering, and real-world applications.",
           section: "Teaching",handler: () => {
               window.location.href = "/teaching/Fall-2024_CSE-411-%20Machine-Learning/";
+            },},{id: "teaching-machine-learning-lab",
+          title: 'Machine Learning Lab',
+          description: "An introduction to machine learning lab techniques, covering preprocessing, pattern mining, classification, clustering, and real-world applications.",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/Fall-2024_CSE-412-%20Machine-Learning-Lab/";
             },},{id: "teaching-artificial-intelligence",
           title: 'Artificial Intelligence',
           description: "An introduction to AI covering search, logic, planning, uncertainty, and NLP with real-world applications.",
