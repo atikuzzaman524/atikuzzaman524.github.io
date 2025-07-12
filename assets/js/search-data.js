0eebb349233762dba5e96a1d42a2b9047386ecd3
atikuzzaman524 <18502839+atikuzzaman524@users.noreply.github.com>
2025-07-12 16:35:12 +0000

    Deploying to gh-pages from @ atikuzzaman524/atikuzzaman524.github.io@ba88a30a040ae264dfb112e5e7bd662527bb45f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,7 +41,7 @@ ninja.data = [{
       
         title: "Markov Decision Processes (MDPs)",
       
-      description: "Convolutional Neural Networks (CNNs) a blog post with jupyter notebook",
+      description: "Markov Decision Processes (MDPs) a blog post with jupyter notebook",
       section: "Posts",
       handler: () => {
         
